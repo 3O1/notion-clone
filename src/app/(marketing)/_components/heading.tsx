84b099775fa9c91297ae3220d01d22c34bd86fff
3O1 <james@meetz.so>
2024-12-16 16:55:06 -0500

    Added sidebar

--- src/app/(marketing)/_components/heading.tsx
+++ src/app/(marketing)/_components/heading.tsx
@@ -2,8 +2,14 @@
 
 import { ArrowRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { useConvexAuth } from 'convex/react';
+import { Spinner } from '@/components/spinner';
+import Link from 'next/link';
+import { SignInButton } from '@clerk/clerk-react';
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
@@ -16,9 +22,27 @@ export const Heading = () => {
         better, faster work happens.
       </h3>
 
-      <Button>
-        Enter Notion <ArrowRight className="w-4 h-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex items-center justify-center w-full">
+          <Spinner size="lg" />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Notion <ArrowRight className="w-4 h-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Notion free <ArrowRight className="w-4 h-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
