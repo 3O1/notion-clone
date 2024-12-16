a12b12f9b61defa204bb7323597e4e160410c3f7
3O1 <james@meetz.so>
2024-12-16 13:56:01 -0500

    Added sign in & sign up

--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { useConvexAuth } from 'convex/react';
 import { useScrollTop } from '@/hooks/use-scroll-top';
 import { ModeToggle } from '@/components/mode-togge';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 import { Logo } from './logo';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/spinner';
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +23,28 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="flex items-center justify-between w-full md:ml-auto md:justify-end gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode="modal">
+              <Button size="sm">Get Notion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Notion</Link>
+            </Button>
+            <UserButton afterSwitchSessionUrl="/" afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
